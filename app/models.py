23f32d2fa5672534f4a0a29145c7b428d02c7767
JoyMarocho <joymarocho@gmail.com>
2022-05-09 09:35:20 +0300

    creating a user model, Installing files and virtual env

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from . import db
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True, index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+        
