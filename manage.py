007e89f02872720e47b23224e2763e4480e2df46
JoyMarocho <joymarocho@gmail.com>
2022-05-09 23:09:04 +0300

    initializing the migrate extension

--- manage.py
+++ manage.py
@@ -1,6 +1,21 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User,Role,Comment
+from flask_migrate import Migrate,MigrateCommand
+
+app = create_app('test')
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+manager.add_command('db',MigrateCommand)
+#manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 
