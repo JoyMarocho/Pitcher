98ce04034a5b467f04807163f5a145d9732f319c
JoyMarocho <joymarocho@gmail.com>
2022-05-09 08:50:21 +0300

    creating configuration

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+    
+    SECRET_KEY = 'thisissecret'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bigbaby:marocho@localhost/pitchcollections'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://bigbaby:marocho@localhost/pitchcollections_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://bigbaby:marocho@localhost/pitchcollections'
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
