be1f59473c5d4cd0e62f8a311e1d01a7fd29f7b4
JoyMarocho <joymarocho@gmail.com>
2022-05-10 19:31:14 +0300

    validating if user exists

--- app/main/views.py
+++ app/main/views.py
@@ -36,3 +36,12 @@ def update_pic(uname):
         user.profile_pic_path = path
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
