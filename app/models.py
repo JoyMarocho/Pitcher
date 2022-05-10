094c4cf1edb1d96b89e266a3b7e65530a67b8e73
JoyMarocho <joymarocho@gmail.com>
2022-05-10 18:41:46 +0300

    creating tables and functions

--- app/models.py
+++ app/models.py
@@ -1,42 +1,72 @@
-from flask import render_template,redirect,url_for,flash,request
-from . import auth
-from ..models import User
-from .forms import LoginForm,RegistrationForm
-from flask_login import login_user,logout_user,login_required
-from ..import db
-from ..email import mail_message
+from enum import unique
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
-@auth.route('/login')
-def login():
-    login_form = LoginForm()
-    if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True, index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+        
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        flash('Invalid username or Password')
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
-        title = "pitcher login"
-        return render_template('auth/login.html',login_form =login_form,title=title)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
 
-@auth.route('/register',methods = ["GET","POST"])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data,username=form.username.data,password=form.password.data)
-        db.session.add(user)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Role(db.Model):
+    '''
+    Role class that defines all other different roles.
+    '''
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role', lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_review = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
         db.session.commit()
 
-        mail_message("Welcome to Pitcher","email/welcome_user",user.email,user=user)
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
-
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for("main.index"))
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
