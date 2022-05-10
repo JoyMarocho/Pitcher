e72d71cf9af6d5a30887a4fb28fc131e44db3561
JoyMarocho <joymarocho@gmail.com>
2022-05-10 19:29:53 +0300

    creating a function for update pic

--- app/main/views.py
+++ app/main/views.py
@@ -25,3 +25,14 @@ def new_comment(id):
 
     title = f'{pitch.title} comment'
     return render_template('new_comment.html',title=title,comment_form=form,pitch=pitch)
+
+@main.route('/user/<uname>/update/pic',methods=['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
