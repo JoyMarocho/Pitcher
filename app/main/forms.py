20abefa3d090a7865a677d0b3e00c15586b8c8db
JoyMarocho <joymarocho@gmail.com>
2022-05-11 17:41:24 +0300

    debugging and reinstallation

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,14 +1,14 @@
 from flask.app import Flask
 from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 class CommentForm(FlaskForm):
     
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
+    title = StringField('Review title',validators=[DataRequired()])
+    review = TextAreaField('Movie review', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    bio = TextAreaField('Tell us about you.', validators=[DataRequired()])
     submit = SubmitField('Submit')
